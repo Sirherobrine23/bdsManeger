7b61888c81a2899721a570d06d53275fea5b7f0e
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-01-31 15:12:43 -0300

    deocker test fix

--- new_script/bds_download.js
+++ new_script/bds_download.js
@@ -6,7 +6,6 @@ module.exports = (Vdown) => {
         if (bds.platform === "bedrock"){
             const system = bds.system
             var mine_name = `bedrock.zip`
-            var server_DIR = bds.bds_dir_bedrock;
             if (system === 'linux'){
                 var versions_get = versions.bedrock[Vdown].url_linux;
             } else {
@@ -18,14 +17,13 @@ module.exports = (Vdown) => {
             var downloadBDSchild = exec(`curl ${versions_get} --output ${mine_name}`, {
                 cwd: `${bds.tmp_dir}`
             });
-            var ZIP_FILE_PATH = `${bds.tmp_dir}/${mine_name}`;
-            downloadBDSchild.stdout.on("data", function(data){
-                console.log(data)
-            })
+            
+            downloadBDSchild.stdout.on("data", function(data){console.log(data)})
             downloadBDSchild.on("exit", function (code) {
                 if (code === 0) {
                     console.log(`Download zip file success`);
-
+                    var server_DIR = bds.bds_dir_bedrock;
+                    var ZIP_FILE_PATH = require("path").join(bds.tmp_dir, mine_name);
                     var AdmZip = require("adm-zip");
                     const fs = require("fs")
                     if (fs.existsSync(`${server_DIR}/server.properties`)){var _old = true;var old1 = fs.readFileSync(`${server_DIR}/server.properties`, "utf-8");}
@@ -34,27 +32,19 @@ module.exports = (Vdown) => {
                     if (fs.existsSync(`${server_DIR}/valid_known_packs.json`)){var _old4 = true;var old4 = fs.readFileSync(`${server_DIR}/valid_known_packs.json`, "utf-8");};
                     // Unzip 
                     var zip = new AdmZip(ZIP_FILE_PATH);
-                    var zipEntries = zip.getEntries();
-                    var totalfiles = zipEntries.length
-                    zipEntries.forEach(function(zipEntry) {
-                        console.log(zipEntry.entryName)
-                        totalfiles--
-                        if (totalfiles === 0){
-                            if (_old){fs.writeFileSync(`${server_DIR}/server.properties`, old1);}
-                            if (_old2){fs.writeFileSync(`${server_DIR}/permissions.json`, old2);}
-                            if (_old3){fs.writeFileSync(`${server_DIR}/whitelist.json`, old3);}
-                            if (_old4){fs.writeFileSync(`${server_DIR}/valid_known_packs.json`, old4);};
-                            const docker_exit = process.env.BDS_DOCKER_IMAGE
-                            console.log(docker_exit)
-                            if (docker_exit == "true"){
-                                console.log(`going out`)
-                                process.exit(0)
-                            }
-                        }   
-                    });
                     zip.extractAllTo(server_DIR, true);
-                    console.log("Downlod Sucess"); // End Unzip
+                    console.log("Extract Sucess"); // End Unzip
                     localStorage.setItem("Downlaod_sucess", "yes")
+                    if (_old){fs.writeFileSync(`${server_DIR}/server.properties`, old1);}
+                    if (_old2){fs.writeFileSync(`${server_DIR}/permissions.json`, old2);}
+                    if (_old3){fs.writeFileSync(`${server_DIR}/whitelist.json`, old3);}
+                    if (_old4){fs.writeFileSync(`${server_DIR}/valid_known_packs.json`, old4);};
+                    const docker_exit = process.env.BDS_DOCKER_IMAGE
+                    console.log(docker_exit)
+                    if (docker_exit == "true"){
+                        console.log(`going out`)
+                        process.exit(0)
+                    }
                 } else {
                     localStorage.setItem("Download_sucess", "no")
                     throw new error(`Could not download`);
