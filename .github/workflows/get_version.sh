b94b7d55096644875d1fc656dec8f345fbad93a2
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-02-09 14:49:49 -0300

    push

--- .github/workflows/get_version.sh
+++ .github/workflows/get_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-v=`cat package.json | jq '.version' | tr "\"" " "`
-d=`cat package.json | jq '.dependencies'|tr "\n" " "|sed "s|\^||g"|sed "s|  ||g"`
+v=`cat package.json | jq '.version'|sed 's|"||g'`
+d=`cat package.json | jq '.dependencies' | tr "\n" " " | sed 's|\^||g' | sed 's|  ||g'`
 echo "bds_api_version=${v}" >> $GITHUB_ENV
-echo "bds_api_depe=${d} >> $GITHUB_ENV
+echo "bds_api_depe=${d}" >> $GITHUB_ENV
