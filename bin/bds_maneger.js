7cfc22dda1f2a7501055f9b15dbcc82e027139d9
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-04-21 00:45:30 -0300

    push

--- bin/bds_maneger.js
+++ bin/bds_maneger.js
@@ -11,35 +11,38 @@ const {join} = require("path")
 const bds_config = bds.bds_config
 process.title = "Bds-Maneger";
 if (argv.h || argv.help) {
-  console.log([
-    "usage: bds_maneger [options]",
-    "",
-    "options:",
-    "  -p --platform        Select server platform",
-    "  -V --server_version  server version to install, default \"latest\"",
-    "  -h --help            Print this list and exit.",
-    "  -v --version         Print the version and exit."
-  ].join("\n"));
-  process.exit();
+    console.log([
+        "usage: bds_maneger [options]",
+        "",
+        "options:",
+        "  -s --start           Start Server",
+        "  -p --platform        Select server platform",
+        "  -V --server_version  server version to install, default \"latest\"",
+        "  -h --help            Print this list and exit.",
+        "  -v --version         Print the version and exit."
+    ].join("\n"));
+    process.exit();
 }
 
 var server =  (argv.p || argv.platform );
 var version = (argv.v || argv.version);
 var bds_version = (argv.V || argv.server_version);
+var start = (argv.s || argv.server_version);
 if (version) {
-  console.info("Bds Maneger core with version " + require("../package.json").version);
-  process.exit();
+    console.info("Bds Maneger core with version " + require("../package.json").version);
+    process.exit();
 }
 if (server) {
-  console.log(server);
-  try {
-    if (server === "JAVA"||server === "java") {bds.change_platform("java");bds.platform="java"}
-    else if (server === "BEDROCK"||server === "bedrock") {bds.change_platform("bedrock");bds.platform="bedrock"}
-    else if (server === "") {bds.change_platform("bedrock");bds.platform="bedrock"}
-    else console.warn("Invalid platform, supported platforms are java and bedrock")
-  } finally {
-    process.exit()
-  }
+    console.log(server);
+    try {
+        if (server === "JAVA"||server === "java") {bds.change_platform("java");}
+        else if (server === "BEDROCK"||server === "bedrock") {bds.change_platform("bedrock");}
+        else if (server === "POCKETMINE-MP" || server === "pocketmine" || server === "pocketmine" || server === "POCKETMINE") {bds.change_platform("pocketmine");}
+        else if (server === "") {bds.change_platform("bedrock");bds.platform="bedrock"}
+        else console.warn("Invalid platform, supported platforms are bedrock, java and pocketmine")
+    } finally {
+        process.exit()
+    }
 }
 
 // check is installed
@@ -47,37 +50,38 @@ var server_exec;
 if (bds_config.bds_platform === "bedrock") {var file; if (process.platform === "linux") file = "bedrock_server"; else file = "bedrock_server.exe"; server_exec = join(bds.bds_dir_bedrock, file)}
 else server_exec = join(bds.bds_dir_java, "MinecraftServerJava.jar");
 if (!(existsSync(server_exec))) {
-  console.warn("Installing the latest version of the server, anything you can install with bds_maneger -V [version], wait ...");
-  bds_version = "latest"
+    console.warn("Installing the latest version of the server, anything you can install with bds_maneger -V [version], wait ...");
+    bds_version = "latest"
 }
 
 if (bds_version){
-  try {
-    process.env.BDS_DOCKER_IMAGE = true
-    bds.download(bds_version, true)
-  } catch (error) {
-    console.error(error)
-    process.exit(165)
-  }
+    try {
+        process.env.BDS_DOCKER_IMAGE = true
+        bds.download(bds_version, true)
+    } catch (error) {
+        console.error(error)
+        process.exit(165)
+    }
+} else {
+    if (start) {
+        console.info("Send a \"stop\" command to stop the server and exit")
+        console.info("Use CTRL + C to force exit")
+        const bds_server = bds.start()
+        bds_server.stdout.on("data", function (data){
+            console.log(data)
+        })
+        bds_server.on("exit", function (code){
+            console.log("leaving the server")
+            process.exit(code)
+        })
+        bds.api()
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        rl.on("SIGINT", (input) => {
+            if (input === "stop") {rl.close();console.log("------------------------ Going out ------------------------");}
+            bds.command(input)
+        });
+    }
 }
-else {
-  console.info("Send a \"stop\" command to stop the server and exit")
-  console.info("Use CTRL + C to force exit")
-  const bds_server = bds.start()
-  bds_server.stdout.on("data", function (data){
-    console.log(data)
-  })
-  bds_server.on("exit", function (code){
-    console.log("leaving the server")
-    process.exit(code)
-  })
-  bds.api()
-}
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-rl.on("line", (input) => {
-  if (input === "stop") {rl.close();console.log("------------------------ Going out ------------------------");}
-  bds.command(input)
-});
--- scripts/download.js
+++ scripts/download.js
@@ -1,140 +1,123 @@
 var AdmZip = require("adm-zip");
-const { warn } = require("console");
-const {writeFileSync, existsSync, readFileSync, readdirSync} = require("fs");
+const { warn, info } = require("console");
+const { writeFileSync, existsSync, readFileSync, readdirSync } = require("fs");
 const { join, resolve } = require("path");
-const {bds_config, bds_dir_bedrock, bds_dir_java, platform_version_update, valid_platform, PHPurlNames, bds_dir_pocketmine, PHPbinsUrls} = require("../index")
-const bdsSystem = require("../index").system
+const { bds_config, bds_dir_bedrock, bds_dir_java, platform_version_update, valid_platform, bds_dir_pocketmine, PHPbinsUrls } = require("../index")
 const response = require("../index").SERVER_URLs
 const commandExists = require("command-exists").sync
 module.exports = function (version, force_install) {
     if (force_install === true) {
+        info("Bds Maneger core force install")
         bds_config.platform_version.java = "latest";
         bds_config.platform_version.bedrock = "latest"
         bds_config.platform_version.pocketmine = "latest"
     }
-    try {
-        if (version === "") version="latest"
-        if (version === undefined) version="latest"
-        const server_platform = bds_config.bds_platform
-        var url;
-        if (server_platform === "bedrock") {
-            if (valid_platform.bedrock === true){
-                var server_configs, permissions, whitelist
-                if (version === "latest") version = response.bedrock_latest
-                if (existsSync(join(bds_dir_bedrock, "server.properties"))) server_configs = readFileSync(join(bds_dir_bedrock, "server.properties"), "utf8");
-                if (existsSync(join(bds_dir_bedrock, "permissions.json"))) permissions = readFileSync(join(bds_dir_bedrock, "permissions.json"), "utf8")
-                if (existsSync(join(bds_dir_bedrock, "whitelist.json"))) whitelist = readFileSync(join(bds_dir_bedrock, "whitelist.json"), "utf8")
-                if (process.platform === "linux") url = response.bedrock[version].url_linux
-                else if (process.platform === "win32") url = response.bedrock[version].url_windows
-                //else if (process.platform === "darwin") url = response.bedrock[version].url_linux
-                console.log(`Server data publish: ${response.bedrock[version].data}`)
-                if (bds_config.platform_version.bedrock !== version) {
-                    fetch(url).then(response => response.arrayBuffer()).then(response => Buffer.from(response)).then(response => {
-                        console.log("Download Sucess")
-                        const zip_buffer = response
-                        var zip = new AdmZip(zip_buffer);
-                        zip.extractAllTo(bds_dir_bedrock, true)
-                        console.log("Extract Sucess")
-                        if (server_configs) writeFileSync(join(bds_dir_bedrock, "server.properties"), server_configs);
-                        if (permissions) writeFileSync(join(bds_dir_bedrock, "permissions.json"), permissions)
-                        if (whitelist) writeFileSync(join(bds_dir_bedrock, "whitelist.json"), whitelist)
-                        platform_version_update(version)
-                        if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
-                    })
-                } else {
-                    warn("Jumping, installed version")
-                    if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
-                }
-            } else throw Error("Bedrock Not suported")
-        } else if (server_platform === "java") {
-            if (valid_platform.java === true){
-                if (version === "latest") version = response.java_latest
-                if (version !== bds_config.platform_version.java){
-                    url = response.java[version].url
-                    console.log(`Server data publish: ${response.java[version].data}`)
-                    fetch(url).then(response => response.arrayBuffer()).then(response => Buffer.from(response)).then(response => {
-                        console.log("Download Sucess")
-                        writeFileSync(join(bds_dir_java, "MinecraftServerJava.jar"), response, "binary")
-                        console.log("Save sucess");
-                        platform_version_update(version)
-                        if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
-                    })
-                } else {
-                    warn("Jumping, installed version")
-                    if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
-                }
-            } else throw Error("Java not suported")
-        } else  if (server_platform === "pocketmine") {
-            if (valid_platform.pocketmine === true) {
-                if (version === "latest") version = response.PocketMine_latest
-                url = response.PocketMine[version].url
-                console.log(`Server data publish: ${response.PocketMine[version].data}`)
-                console.log(bds_dir_pocketmine);
+    if (version === "") version="latest"
+    if (version === undefined) version="latest"
+    var url;
+
+    // Bedrock Installer Script
+    if (bds_config.bds_platform === "bedrock") {
+        if (valid_platform.bedrock === true){
+            var server_configs, permissions, whitelist;
+            if (version === "latest") version = response.bedrock_latest
+            if (existsSync(join(bds_dir_bedrock, "server.properties"))) server_configs = readFileSync(join(bds_dir_bedrock, "server.properties"), "utf8");
+            if (existsSync(join(bds_dir_bedrock, "permissions.json"))) permissions = readFileSync(join(bds_dir_bedrock, "permissions.json"), "utf8");
+            if (existsSync(join(bds_dir_bedrock, "whitelist.json"))) whitelist = readFileSync(join(bds_dir_bedrock, "whitelist.json"), "utf8");
+            url = response.bedrock[version][process.arch][process.platform]
+            if (response.bedrock[version].data) console.log(`Server data publish: ${response.bedrock[version].data}`)
+            if (bds_config.platform_version.bedrock !== version) {
                 fetch(url).then(response => response.arrayBuffer()).then(response => Buffer.from(response)).then(response => {
-                    writeFileSync(join(bds_dir_pocketmine, "PocketMine-MP.phar"), response, "binary")
-                    console.log("PocketMine-MP.phar saved");
+                    console.log("Download Sucess")
+                    const zip_buffer = response
+                    var zip = new AdmZip(zip_buffer);
+                    zip.extractAllTo(bds_dir_bedrock, true)
+                    console.log("Extract Sucess")
+                    if (server_configs) writeFileSync(join(bds_dir_bedrock, "server.properties"), server_configs);
+                    if (permissions) writeFileSync(join(bds_dir_bedrock, "permissions.json"), permissions)
+                    if (whitelist) writeFileSync(join(bds_dir_bedrock, "whitelist.json"), whitelist)
                     platform_version_update(version)
-                    const binFolder = join(bds_dir_pocketmine, "bin")
-                    var CheckBinPHPFolder;
-                    if (existsSync(binFolder)) CheckBinPHPFolder = false
-                    else if (commandExists("php")) CheckBinPHPFolder = false
-                    else CheckBinPHPFolder = true
-                    if (CheckBinPHPFolder||force_install) {
-                        var urlPHPBin;
-                        for (let nameFile of PHPurlNames){
-                            var archS;
-                            if (process.platform === "linux") if (process.arch === "x64") archS = "x86_64";
-                            if (process.platform === "darwin") if (process.arch === "x64") archS = "x86_64";
-                            if (process.platform === "win32") if (process.arch === "x64") archS = "x64";
-                            var arch = false, system = false;
-                            if (nameFile.includes(bdsSystem)) system = true
-                            if (nameFile.includes(archS)) arch = true
-                            // -*-*-*-*-
-                            console.log({
-                                arch,
-                                system
-                            });
-                            if (arch === true && system === true){
-                                urlPHPBin = PHPbinsUrls[nameFile]
-                            }
-                        }
-                        if (urlPHPBin === undefined) throw Error("File not found")
-                        else {
-                            console.log(urlPHPBin);
-                            fetch(urlPHPBin).then(response => response.arrayBuffer()).then(response => Buffer.from(response)).then(response => {
-                                console.log("Download Sucess")
-                                var zipExtractBin = new AdmZip(response);
-                                zipExtractBin.extractAllTo(bds_dir_pocketmine, true)
-                                console.log("Extract Sucess")
-                                const phpBinFolder = resolve(bds_dir_pocketmine, "bin")
-                                const phpIni = readFileSync(join(phpBinFolder, "php7", "bin", "php.ini"), "utf-8")
-                                const phpIniSplit = phpIni.split("\n")
-                                var check_extension_dir = false
-                                for (let index in phpIniSplit){
-                                    let test = phpIniSplit[index]
-                                    if (test.includes("extension_dir")) check_extension_dir = true;
-                                    console.log(test);
-                                }
-                                if (check_extension_dir) console.log("Skipping php.ini configuration");
-                                else {
-                                    const phpExtensiosnsDir = resolve(bds_dir_pocketmine, "bin/php7/lib/php/extensions")
-                                    const phpExtensiosns = readdirSync(phpExtensiosnsDir)
-                                    var exetensionZen;
-                                    for (let index2 in phpExtensiosns){
-                                        
-                                        if (phpExtensiosns[index2].includes("debug-zts")) exetensionZen = phpExtensiosns[index2]
-                                    }
-                                    phpIniSplit.push(`extension_dir="${resolve(phpExtensiosnsDir, exetensionZen)}"`)
-                                    writeFileSync(join(phpBinFolder, "php7", "bin", "php.ini"), phpIniSplit.join("\n"))
-                                }
-                                if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
-                            })
-                        }
-                    } else if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
+                    if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
                 })
-            } else throw Error("Pocketmine not suported")
-        } else throw Error("Bds maneger Config file error")
-    } catch (error) {
-        console.error(error);
+            } else {
+                warn("Jumping, installed version")
+                if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
+            }
+        } else throw Error("Bedrock Not suported")
     }
+    // java Installer Script
+    else if (bds_config.bds_platform === "java") {
+        if (valid_platform.java === true){
+            if (version === "latest") version = response.java_latest
+            if (version !== bds_config.platform_version.java){
+                url = response.java[version].url
+                console.log(`Server data publish: ${response.java[version].data}`)
+                fetch(url).then(response => response.arrayBuffer()).then(response => Buffer.from(response)).then(response => {
+                    console.log("Download Sucess")
+                    writeFileSync(join(bds_dir_java, "MinecraftServerJava.jar"), response, "binary")
+                    console.log("Save sucess");
+                    platform_version_update(version)
+                    if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
+                })
+            } else {
+                warn("Jumping, installed version")
+                if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
+            }
+        } else throw Error("Java not suported")
+    }
+    // Pocketmine-MP Installer Script
+    else  if (bds_config.bds_platform === "pocketmine") {
+        if (valid_platform.pocketmine === true) {
+            if (version === "latest") version = response.PocketMine_latest
+            url = response.PocketMine[version].url
+            console.log(`Server data publish: ${response.PocketMine[version].data}`)
+            console.log(bds_dir_pocketmine);
+            fetch(url).then(response => response.arrayBuffer()).then(response => Buffer.from(response)).then(response => {
+                writeFileSync(join(bds_dir_pocketmine, "PocketMine-MP.phar"), response, "binary")
+                console.log("PocketMine-MP.phar saved");
+                platform_version_update(version)
+                const binFolder = join(bds_dir_pocketmine, "bin")
+                var CheckBinPHPFolder;
+                if (existsSync(binFolder)) CheckBinPHPFolder = false ;else if (commandExists("php")) CheckBinPHPFolder = false ;else CheckBinPHPFolder = true
+                if (CheckBinPHPFolder||force_install) {
+                    var urlPHPBin;
+                    try {
+                        urlPHPBin = PHPbinsUrls[process.platform][process.arch];
+                    } catch (error) {
+                        throw new Error({
+                            error: "Not found to platform",
+                            platform: process.platform,
+                            arch: process.arch,
+                            Others_support: PHPbinsUrls.other
+                        })
+                    }
+                    fetch(urlPHPBin).then(res => {if (res.ok) return res; else throw new Error(res)}).then(response => response.arrayBuffer()).then(response => Buffer.from(response)).then(php => {
+                        console.log("Downloading PHP Binaries");
+                        const zipExtractBin = new AdmZip(php);
+                        // Extract bin
+                        zipExtractBin.extractAllTo(bds_dir_pocketmine, true)
+                        console.log("Successfully extracting the binaries")
+                        // Check Configs and others
+                        const phpFolder = resolve(bds_dir_pocketmine, "bin")
+                        const phpConfigInit = readFileSync(join(phpFolder, "php7", "bin", "php.ini"), "utf-8").split(/\r\?\n/g);
+                        // Check Php bin folder and bins
+                        var check_extension_dir = false;
+                        for (let index of phpConfigInit) if (index.includes("extension_dir")) check_extension_dir = true;
+                        // Post check extension_dir
+                        if (check_extension_dir) console.log("Skipping php.ini configuration");
+                        else {
+                            const phpExtensiosnsDir = resolve(bds_dir_pocketmine, "bin/php7/lib/php/extensions")
+                            const phpExtensiosns = readdirSync(phpExtensiosnsDir)
+                            var exetensionZen;
+                            for (let index of phpExtensiosns) if (index.includes("debug-zts")) exetensionZen = index
+                            phpConfigInit.push(`extension_dir="${resolve(phpExtensiosnsDir, exetensionZen)}"`)
+                            writeFileSync(join(phpFolder, "php7", "bin", "php.ini"), phpConfigInit.join("\n"))
+                        }
+                        if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
+                        // End Phph bin
+                    })
+                } else if (process.env.BDS_DOCKER_IMAGE === "true") process.exit(0);
+            })
+        } else throw Error("Pocketmine not suported")
+    } else throw Error("Bds maneger Config file error")
 }
