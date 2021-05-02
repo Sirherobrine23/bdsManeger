70c1159a1cbc51285f8040759b39d9cd22008707
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-05-01 21:33:11 -0300

    Update commandExist.js

--- commandExist.js
+++ commandExist.js
@@ -1,26 +1,29 @@
+const { execSync } = require("child_process");
 const { readdirSync, existsSync } = require("fs");
-function getBins() {
-    var PATHs;
-    if (process.platform === "win32") PATHs = process.env.PATH.split(/;/g);
-    else PATHs = process.env.PATH.split(/:/g);
-    const bin = []
-    for (let path of PATHs){
-        if (existsSync(path))
-            for (let binS of readdirSync(path)) {
-                bin.push(binS)
-            }
-    }
-    return bin
-}
 
 function commdExist(command){
-    let bin = getBins()
-    for (let index of bin) {
-        if (index === command) return true;
-        else if (process.platform === "win32" && !command.includes(".exe")) {if (index === command+".exe") return true;}
+    if (process.platform === "linux" || process.platform === "darwin" || process.platform === "android") {try {execSync(`command -v ${command}`);return true} catch (error) {return false}}
+    else if (process.platform === "win32") {try {execSync(`where ${command} > nul 2> nul`);return true} catch (error) {return false}}
+    else {
+        var PATHs;
+        const bin = []
+        if (process.platform === "win32") PATHs = process.env.PATH.split(/;/g);else PATHs = process.env.PATH.split(/:/g);
+        
+        for (let path of PATHs)
+        if (existsSync(path))
+            for (let binS of readdirSync(path)) bin.push(binS);
+        for (let index of bin) {
+            if (process.platform === "linux") {if (index === command) return true}
+            else if (process.platform === "win32") {
+                if (!command.includes(".exe", ".cmd", ".bat", ".EXE", ".CMD", ".BAT")) {
+                    for (let test of [".exe", ".cmd", ".bat", ".EXE", ".CMD", ".BAT"])
+                    if (index === `${command}${test}`) return true;
+                }
+            }
+        }
+        return false
     }
-    return false
 }
 
 module.exports = commdExist
-module.exports.sync = commdExist
+module.exports.sync = 
