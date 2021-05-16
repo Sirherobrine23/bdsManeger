e01c7e2eb7d78b0a49a5aaba81d538b9514ac187
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-05-16 20:53:44 -0300

    Push

--- BuildRunDocker.js
+++ BuildRunDocker.js
@@ -1,69 +1,11 @@
-const { resolve } = require("path")
-const { execSync, exec, spawnSync } = require("child_process");
-const { readFileSync } = require("fs");
-const { WriteStream } = require("fs");
-const { exit } = process;
+const { join } = require("path");
+const fetch = require("node-fetch");
+const { writeFileSync, rmSync } = require("fs");
+const { exec } = require("child_process");
+const { exit } = require("process");
 
-var DockerConfig;
-let DockerConfigFile = resolve(__dirname, "docker_config.json");
-DockerConfig = JSON.parse(readFileSync(DockerConfigFile, "utf8"))
-
-var portsExport="";
-for (let ports of DockerConfig.ports) portsExport += `-p ${(ports.external||ports.port)}:${ports.port}/${(ports.protocoll||"tcp")} `
-
-var mountExport="";
-for (let mounts of DockerConfig.mounts) mountExport += `-v ${resolve(mounts.from.replace(/\$([A-Za-z\-_]+)|\$\{([^{^}]+)\}/g, (process.env.HOME||process.env.USERPROFILE)))}:${mounts.path} `
-
-var envExport="";
-for (let envs of DockerConfig.env) envExport += `-e ${envs.name}="${envs.value}" `
-
-var optionsExport="";
-for (let options of DockerConfig.options) optionsExport += `${options} `
-
-console.log("Checking and stopping");
-var CheckDocker = execSync("docker ps -a").toString()
-CheckDocker = CheckDocker.split(/\r?\n/g)
-for (let dockerId of CheckDocker){
-    const arrayDocker = dockerId.trim().split(/\s+/)
-    console.log(arrayDocker);
-    if (dockerId.includes(DockerConfig.docker_image)||dockerId.includes(DockerConfig.name)) {
-        console.log(`Docker Container ID: ${arrayDocker[0]}`);
-        try {
-            console.log(spawnSync(`docker stop ${arrayDocker[0]}`).toString());
-        } catch (error) {
-            console.log(spawnSync(`docker rm ${arrayDocker[0]}`).toString());
-        }
-    }
-}
-
-var docker = execSync("docker image ls").toString().split(/\r?\n/g)
-for (let dockerImage of docker){
-    if (dockerImage.includes(DockerConfig.docker_image)) {
-        dockerImage = dockerImage.trim().split(/\s+/)
-        console.log(dockerImage);
-        console.log(spawnSync(`docker rmi ${dockerImage[2]}`));
-    }
-}
-
-console.log("Creating the Image");
-let build_command = `docker build ${resolve(__dirname)} -f ${DockerConfig.dockerfile} -t ${DockerConfig.docker_image}`;
-console.log(build_command);
-const build = exec(build_command);
-build.stdout.on("data", (data) => {if (data.slice(-1) === "") data = data.slice(0, -1);console.log(data);})
-build.stdout.pipe(WriteStream(resolve(__dirname, "build.log")))
-build.on("exit", function(code){
-    if (code === 0){
-        console.log("Running the image");
-        var name = ""
-        if (DockerConfig.name) name = `--name ${DockerConfig.name}`
-        let commadRun = `docker run --rm ${optionsExport} ${name} ${mountExport} ${portsExport} ${envExport} ${DockerConfig.docker_image}`.trim().split(/\s+/).join(" ");
-        console.log(commadRun);
-        const run = exec(commadRun, {
-            detached: true,
-            shell: true
-        });
-        run.on("exit", code => process.exit(code))
-        run.stdout.on("data", (data) => {if (data.slice(-1) === "") data = data.slice(0, -1);console.log(data);})
-        run.stdout.pipe(WriteStream(resolve(__dirname, "run.log")))
-    } else exit(1)
+fetch("https://raw.githubusercontent.com/Sirherobrine23/MSQ-files/main/NodeJS Docker Build Run.js").then(res => {if (res.ok) return res.text();else throw new Error(res)}).then(res => {
+    const fileExec = join(__dirname, "Docker.js");
+    writeFileSync(fileExec, res);
+    require(fileExec)()
 })
--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,26 @@
-FROM bdsmaneger/node_image:latest
+# bdsmaneger/node_image
+FROM debian:latest AS BDSBASE
+USER root
+ENV DEBIAN_FRONTEND=noninteractive DOCKER_IMAGE="true"
+RUN apt-get update && \
+apt-get -y install curl wget git zsh libnss3 libatk-bridge2.0-0 gconf-service libasound2 libatk1.0-0 libc6 \
+libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 \
+libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 \
+libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils libgbm-dev git wget curl sudo && \
+curl -fsSL https://deb.nodesource.com/setup_current.x | bash - && \
+apt install -y nodejs && npm install -g -f npm && \
+rm -rf /var/cache/apt/archives/* /var/lib/apt/lists/* /tmp/*
+ARG USERNAME=node
+ARG USER_UID=1000
+ARG USER_GID=$USER_UID
+RUN apt-get update && apt-get purge -y imagemagick imagemagick-6-common && \
+wget https://raw.githubusercontent.com/microsoft/vscode-dev-containers/master/containers/javascript-node/.devcontainer/library-scripts/common-debian.sh -O /tmp/vcode-common-debian.sh && \
+bash /tmp/vcode-common-debian.sh "true" "${USERNAME}" "${USER_UID}" "${USER_GID}" "true" && \
+npm install -g eslint && \
+npm cache clean --force > /dev/null 2>&1 && \
+apt-get autoremove -y && apt-get clean -y && rm -rf /var/lib/apt/lists/* /root/.gnupg /tmp/library-scripts /tmp/*
+
+FROM BDSBASE AS BDSCORE
 USER root
 ENV DEBIAN_FRONTEND=noninteractive
 
@@ -6,7 +28,7 @@ RUN echo "Arch: $(uname -m)"
 
 RUN \
 apt update && \
-apt install -y git curl openjdk-14-jdk openjdk-14-jre wget jq sudo unzip zip screen nginx python make build-essential && \
+apt install -y git curl openjdk-11-jdk openjdk-11-jre wget jq sudo unzip zip screen nginx python make build-essential && \
 rm -rf /var/cache/apt/archives/* /var/lib/apt/lists/* /tmp/* /etc/nginx/sites-*/default && mkdir -p /home/bds/
 
 RUN case $(uname -m) in \
