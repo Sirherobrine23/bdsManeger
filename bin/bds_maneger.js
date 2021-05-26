050c9b65183e4ccf23abc8ee58a4569453b5e905
Matheus sampaio Queiroga <srherobrine20@gmail.com>
2021-05-25 21:05:23 -0300

    Bin

--- bin/bds_maneger.js
+++ bin/bds_maneger.js
@@ -1,22 +1,38 @@
 #!/usr/bin/node --no-warnings
-process.env.IS_BIN_BDS = true;process.title = "Bds Maneger CLI";
+process.env.IS_BIN_BDS = true;
 process.env.IS_BDS_CLI = true
-const bds = require("../index")
+if (process.platform === "win32") process.title = "Bds Maneger CLI"; else process.title = "Bds_Manger_CLI"
 const readline = require("readline");
+const bds = require("../index");
 const { bds_dir } = require("../bdsgetPaths");
-var argv = require("minimist")(process.argv.slice(2));
+const commandExits = require("../commandExist");
 
-if (Object.getOwnPropertyNames(argv).length === 1 || Object.getOwnPropertyNames(argv).length === 0) argv.help = true
-const server =  (argv.p || argv.platform ),
-    version = (argv.v || argv.version),
-    SystemCheck = (argv.S || argv.system_info),
-    bds_version = (argv.d || argv.server_download),
-    start = (argv.s || argv.server_version)
+// Bds Maneger ArgV
+const argv = require("minimist")(process.argv.slice(2));
+if (Object.getOwnPropertyNames(argv).length <= 1) argv.help = true
+const
+server =  (argv.p || argv.platform ),
+version = (argv.v || argv.version),
+SystemCheck = (argv.S || argv.system_info),
+bds_version = (argv.d || argv.server_download),
+start = (argv.s || argv.server_version),
+help = (argv.h || argv.help),
+kill = (argv.k || argv.kill)
 
-console.log(process.argv);
+// Bds kill
+if (kill) bds.kill();
+
+// Set Bds Platform
+if (server) {
+    if (server === "BEDROCK"||server === "bedrock") bds.change_platform("bedrock");
+    else if (server === "POCKETMINE-MP" || server === "pocketmine" || server === "pocketmine" || server === "POCKETMINE") bds.change_platform("pocketmine");
+    else if (server === "JAVA"||server === "java") bds.change_platform("java");
+    else if (server === "JSPrismarine" || server === "JSPRISMARINE" || server === "jsprismarine") bds.platform_update("jsprismarine");
+    else console.log("Add one of the valid platforms: bedrock, pocketmine, java, jsprismarine");
+}
 
 // Bds Maneger CLI Help
-if (argv.h || argv.help) {
+if (help) {
     let help = [
         "usage: bds_maneger [options]",
         "",
@@ -35,12 +51,24 @@ if (argv.h || argv.help) {
 
 // Get Bds Core Version
 if (version) {
-    console.info("Bds Maneger core with version " + bds.package_json.version);
+    const Info = [
+        `Bds Maneger Core version: ${bds.package_json.version}`,
+        "",
+        "****************** Bds Maneger Core contributors ******************",
+        "",
+    ]
+    for (let contri of bds.extra_json.contributors) {
+        Info.push(`********* ${contri.name} *********`)
+        if (contri.email) Info.push(`* ${contri.email}`)
+        if (contri.url) Info.push(`* ${contri.url}`)
+        Info.push("*")
+        Info.push("*********")
+    }
+    console.log(Info.join("\n"));
     process.exit();
 }
 
 if (SystemCheck) {
-    const commandExits = require("../commandExist")
     var checkothearch = "";
     if (process.platform === "linux" && bds.arch !== "x64"){checkothearch = `qemu-x86_64-static is installed to emulate an x64 system: ${commandExits("qemu-x86_64-static")}\n`}
     if (process.platform === "android" && bds.arch !== "x64"){checkothearch = `qemu-x86_64 is installed to emulate an x64 system: ${commandExits("qemu-x86_64")}\n`}
@@ -62,18 +90,6 @@ if (SystemCheck) {
     ];
     console.log(help.join("\n"))
     process.exit(0)
-}
-
-// Bds kill
-if (argv.k || argv.kill ) bds.kill();
-
-// Set Bds Platform
-if (server) {
-    if (server === "BEDROCK"||server === "bedrock") bds.change_platform("bedrock");
-    else if (server === "POCKETMINE-MP" || server === "pocketmine" || server === "pocketmine" || server === "POCKETMINE") bds.change_platform("pocketmine");
-    else if (server === "JAVA"||server === "java") bds.change_platform("java");
-    else if (server === "JSPrismarine" || server === "JSPRISMARINE" || server === "jsprismarine") bds.platform_update("jsprismarine");
-    else console.log("Add one of the valid platforms: bedrock, pocketmine, java, jsprismarine");
 }
 
 // Download server
