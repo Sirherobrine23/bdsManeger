1095e21aee97f662be85cd0e48212e9a95f8bf00
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2021-09-28 15:29:56 -0300

    Publish 1.14.0 (2)

--- .Build/RemoveVersions.js
+++ .Build/RemoveVersions.js
@@ -3,24 +3,26 @@ const fs = require("fs");
 const path = require("path");
 const cli_color = require("cli-color");
 
-// Fetch
-if (typeof fetch === "undefined") global.fetch = require("node-fetch");
-
-fetch("https://registry.npmjs.org/@the-bds-maneger/core").then(res => res.json()).then(data => {
-    data.versions = Object.getOwnPropertyNames(data.versions).filter(version => /[0-9]+\.[0-9][0-9][0-9]/.test(version) && version !== data["dist-tags"].dev && version !== data["dist-tags"].latest)
-    fs.writeFileSync(path.resolve(__dirname, "Releases.json"), JSON.stringify(data, null, 2));
-    const Package = require("../package.json");
-    data.versions.map(version => {
-        const cmd = `npm unpublish ${Package.name}@${version}`;
-        console.log(cli_color.yellow(cmd));
-        try {
-            child_process.execSync(cmd).toString()
-            console.log(cli_color.green(`Sucess to remove ${Package.name}@${version}`, "\n"));
-            return cmd;
-        } catch (e) {
-            console.log(cli_color.red(`Failed to remove package: ${Package.name}@${version}`), "\n");
-            return version;
-        }
+async function Render() {
+    // Fetch
+    if (typeof fetch === "undefined") global.fetch = (await import("node-fetch")).default;
+    fetch("https://registry.npmjs.org/@the-bds-maneger/core").then(res => res.json()).then(data => {
+        data.versions = Object.getOwnPropertyNames(data.versions).filter(version => /[0-9]+\.[0-9][0-9][0-9]/.test(version) && version !== data["dist-tags"].dev && version !== data["dist-tags"].latest)
+        fs.writeFileSync(path.resolve(__dirname, "Releases.json"), JSON.stringify(data, null, 2));
+        const Package = require("../package.json");
+        data.versions.map(version => {
+            const cmd = `npm unpublish ${Package.name}@${version}`;
+            console.log(cli_color.yellow(cmd));
+            try {
+                child_process.execSync(cmd).toString()
+                console.log(cli_color.green(`Sucess to remove ${Package.name}@${version}`, "\n"));
+                return cmd;
+            } catch (e) {
+                console.log(cli_color.red(`Failed to remove package: ${Package.name}@${version}`), "\n");
+                return version;
+            }
+        });
+        fs.writeFileSync(path.resolve(__dirname, "Releases.json"), JSON.stringify(data, null, 2));
     });
-    fs.writeFileSync(path.resolve(__dirname, "Releases.json"), JSON.stringify(data, null, 2));
-});
+}
+Render();
