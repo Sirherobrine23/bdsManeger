c90a8b0229811d24a49bea0f69e985cfd4137acb
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2021-09-28 16:12:59 -0300

    Push

--- .Build/SetCIEnvs.js
+++ .Build/SetCIEnvs.js
@@ -1,4 +1,4 @@
-const version = require("./package.json").version;
+const version = require("../package.json").version;
 const ActionsCore = require("@actions/core");
 console.log(version);
 ActionsCore.exportVariable("BdsCoreVersion", version);
