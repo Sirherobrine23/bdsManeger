2299d965591c425d86266d380688689cabf98666
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-04-10 05:39:33 -0300

    fix pocketmine boot

--- src/server.ts
+++ src/server.ts
@@ -162,7 +162,7 @@ export async function Start(Platform: bdsTypes.Platform, options?: startServerOp
       Process.command = path.resolve(ServerPath, "bin/bin/php");
       await child_process.runAsync("chmod", ["a+x", Process.command]);
     }
-    Process.args.push(path.resolve(ServerPath, "PocketMine-MP.phar"));
+    Process.args.push(path.join(ServerPath, "PocketMine.phar"));
   }
 
   if (options) {
