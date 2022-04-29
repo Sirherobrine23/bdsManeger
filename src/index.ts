1176e4d88f6178c027cdb3f25b5d4918ef0fe55c
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-04-28 22:30:30 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,4 @@ import * as downloadServer from "./download_server";
 import * as serverConfig from "./serverConfig";
 import * as bdsTypes from "./globalType";
 import * as platform from "./platform/index";
-export {Server, Backup, downloadServer, serverConfig, bdsTypes, platform};
+export default {Server, Backup, downloadServer, serverConfig, bdsTypes, platform};
