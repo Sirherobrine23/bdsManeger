2bfa09ba90a6541e62a47a1a2161aad14225e854
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-05-25 17:39:40 -0300

    Add backup

---
+++ src/backup/index.ts
@@ -0,0 +1,4 @@
+import * as gitA from "./git";
+import * as zipA from "./zip";
+export const git = gitA;
+export const zip = zipA;
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ import * as downloadServer from "./download_server";
 import * as serverConfig from "./serverConfig";
 import * as bdsTypes from "./globalType";
 import * as platform from "./platform/index";
-export default {Server, downloadServer, serverConfig, bdsTypes, platform};
+import * as backup from "./backup/index";
+export default {Server, downloadServer, serverConfig, bdsTypes, platform, backup};
