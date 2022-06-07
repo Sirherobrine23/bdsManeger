f090b3475d7d03b22a0abcf89f35a4a3c89830ae
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-06-07 19:35:56 +0000

    Stash

--- src/platform/index.ts
+++ src/platform/index.ts
@@ -2,4 +2,22 @@ import * as bedrock from "./bedrock/index";
 import * as pocketmine from "./pocketmine/index";
 import * as java from "./java/index";
 import * as spigot from "./spigot/index";
-export {bedrock, pocketmine, java, spigot}
+
+type globalPlatform = {
+  [platform: string]: {
+    DownloadServer: (version: string) => Promise<{version: string, publishDate: Date, url: string}>,
+    backup: {
+      CreateBackup: () => Promise<Buffer>,
+    }
+  }
+};
+
+bedrock.backup.CreateBackup
+
+const platforms: globalPlatform = {
+  bedrock,
+  pocketmine,
+  java,
+  spigot
+}
+export default platforms
