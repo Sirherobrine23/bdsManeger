fe21dcbe222d7d0f5a88d69c025d6c83ae7438dc
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-09-21 18:19:26 +0000

    Fix bedrock

--- src/pocketmine.ts
+++ src/pocketmine.ts
@@ -3,7 +3,7 @@ import * as fs from "node:fs/promises";
 import * as os from "node:os";
 import * as tar from "tar";
 import { existsSync as  fsExistsSync } from "node:fs";
-import { platformManeger, versionURLs } from "@the-bds-maneger/server_versions";
+import { platformManeger } from "@the-bds-maneger/server_versions";
 import { execFileAsync } from './childPromisses';
 import { logRoot, serverRoot } from "./pathControl";
 import { getBuffer } from "./httpRequest";
@@ -53,18 +53,15 @@ async function buildPhp() {
 }
 
 async function installPhp(): Promise<void> {
-  const file = (await getBuffer(`${versionURLs[0]}/pocketmine/bin?os=${process.platform}&arch=${process.arch}`).then(res => JSON.parse(res.toString("utf8")) as {url: string, name: string}[]))[0];
+  const file = await platformManeger.pocketmine.getPhp({});
   if (!file) return buildPhp();
   if (fsExistsSync(path.resolve(serverPath, "bin"))) await fs.rm(path.resolve(serverPath, "bin"), {recursive: true});
   await fs.mkdir(path.resolve(serverPath, "bin"), {recursive: true});
   // Tar.gz
   if (/tar\.gz/.test(file.name)) {
-    await fs.writeFile(path.join(os.tmpdir(), file.name), await getBuffer(file.url));
+    await fs.writeFile(path.join(os.tmpdir(), file.name), file.buffer);
     await tar.extract({file: path.join(os.tmpdir(), file.name), C: path.join(serverPath, "bin"), keep: true, p: true, noChmod: false});
-  } else {
-    const zip = new AdmZip(await getBuffer(file.url));
-    await promisify(zip.extractAllToAsync)(serverPath, false, true);
-  }
+  } else await promisify((new AdmZip(file.buffer)).extractAllToAsync)(serverPath, false, true);
   if (process.platform === "linux"||process.platform === "android"||process.platform === "darwin") {
     const ztsFind = await Readdir(path.resolve(serverPath, "bin"), [/.*debug-zts.*/]);
     if (ztsFind.length > 0) {
@@ -76,7 +73,7 @@ async function installPhp(): Promise<void> {
     }
   }
   // test it's works php
-  await fs.writeFile(path.join(os.tmpdir(), "test.php"), `<?php echo "Hello World";`);
+  await fs.writeFile(path.join(os.tmpdir(), "bds_test.php"), `<?php echo "Hello World";`);
   await execFileAsync(phpBinPath, ["-f", path.join(os.tmpdir(), "test.php")]).catch(buildPhp);
 }
 
