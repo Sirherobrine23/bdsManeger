e067f556d2541600d254f346d0c0c39e42cf396c
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-10-07 00:09:58 -0300

    Update globalPlatfroms.ts

--- src/globalPlatfroms.ts
+++ src/globalPlatfroms.ts
@@ -171,6 +171,7 @@ export async function actionV2(options: {id: string, platform: bdsPlatform, proc
   // Server avaible to player
   if (options.hooks.serverStarted) serverObject.events.on("log", function(data) {
     return options.hooks.serverStarted(data, onAvaible => {
+      if (!serverObject.serverStarted) return;
       serverObject.serverStarted = onAvaible;
       serverObject.events.emit("serverStarted", onAvaible);
       serverObject.events.removeListener("log", this);
