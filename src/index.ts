26ee4b49b87d203ce06f2c553229a0d6619d7856
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-10-17 18:27:04 +0000

    Export proxy and processLoad

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ export { importBds, exportBds, payloadError } from "./export_import";
 export * as platformPathManeger from "./platformPathManeger"
 export * as globalPlatfroms from "./globalPlatfroms";
 export * as pluginManeger from "./plugin/plugin";
-export * as pluginHooks from "./plugin/hook";
+export * as process_load from "./lib/processLoad";
 export * as PocketmineMP from "./pocketmine";
+export * as pluginHooks from "./plugin/hook";
 export * as Powernukkit from "./pwnuukit";
 export * as PaperMC from "./paper";
 export * as Bedrock from "./bedrock";
 export * as Spigot from "./spigot";
+export * as proxy from "./lib/proxy";
 export * as Java from "./java";
