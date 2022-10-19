43426df07b1919e43e0c06025db0cb3f91948d56
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-10-18 22:23:53 -0300

    export httpRequest (remove .ts)

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export * as process_load from "./lib/processLoad";
 export * as PocketmineMP from "./pocketmine";
 export * as pluginHooks from "./plugin/hook";
 export * as Powernukkit from "./pwnuukit";
-export * as httpRequest from "./lib/httpRequest.ts";
+export * as httpRequest from "./lib/httpRequest";
 export * as PaperMC from "./paper";
 export * as Bedrock from "./bedrock";
 export * as Spigot from "./spigot";
