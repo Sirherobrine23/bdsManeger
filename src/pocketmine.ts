cf78ef177f5129bf2800bfe6585b1a560b162c3f
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-12-11 07:52:05 -0300

    Update pocketmine

--- src/pocketmine.ts
+++ src/pocketmine.ts
@@ -1,4 +1,4 @@
-import { customChildProcess, extendFs, httpRequestGithub, httpRequestLarge } from "@sirherobrine23/coreutils";
+import { customChildProcess, extendFs, httpRequest, httpRequestGithub, httpRequestLarge } from "@sirherobrine23/coreutils";
 import { pathControl, bdsPlatformOptions } from "./platformPathManeger";
 import { existsSync as fsExistsSync, Stats } from "node:fs";
 import { promisify } from "node:util";
@@ -7,6 +7,7 @@ import path from "node:path";
 import fs from "node:fs/promises";
 import os from "node:os";
 import AdmZip from "adm-zip";
+const phpStaticBucket = "https://objectstorage.sa-saopaulo-1.oraclecloud.com/p/0IKM-5KFpAF8PuWoVe86QFsF4sipU2rXfojpaOMEdf4QgFQLcLlDWgMSPHWmjf5W/n/grwodtg32n4d/b/bdsFiles/o/";
 
 export async function listVersions() {
   return (await httpRequestGithub.GithubRelease("pmmp", "PocketMine-MP")).map(data => {
@@ -35,20 +36,19 @@ async function findPhp(serverPath: string, extraPath?: string): Promise<string> 
 }
 
 async function installPhp(serverPath: string, buildFolder: string): Promise<void> {
-  const releases: (httpRequestGithub.githubRelease["assets"][0])[] = [];
-  (await httpRequestGithub.GithubRelease("The-Bds-Maneger", "Build-PHP-Bins")).map(re => re.assets).forEach(res => releases.push(...res));
+  const objectsBucket = (await httpRequest.getJSON<{objects: {name: string}[]}>(phpStaticBucket)).objects.map(file => phpStaticBucket + file.name);
   if (fsExistsSync(path.resolve(serverPath, "bin"))) await fs.rm(path.resolve(serverPath, "bin"), {recursive: true});
   await fs.writeFile(path.join(os.tmpdir(), "bds_test.php"), `<?php echo "Hello World";`);
   if (process.platform === "win32") {
-    let url: string = releases.filter(assert => assert.name.endsWith(".zip")).find(assert => /win32|windows/.test(assert.name))?.browser_download_url;
+    let url: string = objectsBucket.filter(assert => assert.endsWith(".zip")).find(assert => /win32|windows/.test(assert));
     if (!url) throw new Error("Cannnot get php url");
     return promisify((new AdmZip(await httpRequestLarge.saveFile({url}))).extractAllToAsync)(serverPath, false, true);
   } else {
     const fileTest = RegExp(`${process.platform.toLowerCase()}.*${process.arch.toLowerCase()}`);
-    const file = releases.find(re => fileTest.test(re.name.toLowerCase()));
+    const file = objectsBucket.find(re => fileTest.test(re.toLowerCase()));
     if (file) {
-      if (/\.zip/.test(file.name)) await httpRequestLarge.extractZip({url: file.browser_download_url, folderTarget: serverPath});
-      else await httpRequestLarge.tarExtract({url: file.browser_download_url, folderPath: path.join(serverPath, "bin")});
+      if (/\.zip/.test(file)) await httpRequestLarge.extractZip({url: file, folderTarget: serverPath});
+      else await httpRequestLarge.tarExtract({url: file, folderPath: path.join(serverPath, "bin")});
 
       // Update zts
       if (process.platform === "linux"||process.platform === "android"||process.platform === "darwin") {
@@ -63,7 +63,7 @@ async function installPhp(serverPath: string, buildFolder: string): Promise<void
       }
     }
   }
-  
+
   // test it's works php
   await customChildProcess.execFile(await findPhp(serverPath), ["-v"]).catch(err => {
     console.warn(String(err));
